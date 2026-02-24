b9e0c87dd8e5bdd4ab4d3962db9b86b9dc89d37b
cilginc <104676201+cilginc@users.noreply.github.com>
2026-02-25 01:23:18 +0300

    chore(WIP): added some cli flags

--- cmd/root.go
+++ cmd/root.go
@@ -5,18 +5,18 @@ import (
 	"regexp"
 
 	"github.com/cilginc/gochecker/internal/ui"
+	"github.com/cilginc/gochecker/pkg"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gochecker",
-	Short: "A brief description of your application",
-	Long:  ui.Info("I still don't know"),
+	Short: "A fast and modern version checker for software packages",
+	Long: ui.Info(`Gochecker is a high-performance version monitoring tool inspired by nvchecker.
+It allows you to track upstream versions from various sources like GitHub, AUR, 
+and custom web pages using Go's powerful concurrency model.`),
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -32,7 +32,40 @@ func init() {
 		}
 	}
 
-	rootCmd.PersistentFlags().Bool("no-color", false, "disable colored output")
+	f := rootCmd.PersistentFlags()
+
+	f.StringP(
+		"config",
+		"c",
+		pkg.DEFAULT_CONFIG_FILE,
+		"Path to the configuration file containing software entries",
+	)
+
+	f.String(
+		"version-file",
+		pkg.DEFAULT_VERSIONS_FILE,
+		"Path to the versions file containing software entries",
+	)
+
+	// f.StringP(
+	// 	"keyfile",
+	// 	"k",
+	// 	"keyfile.toml",
+	// 	"Path to the file containing API tokens and authentication keys",
+	// )
+
+	f.StringP(
+		"output",
+		"o",
+		"text",
+		"Set the output format; options are 'text', 'json' or 'yaml'",
+	)
+
+	f.Bool(
+		"no-color",
+		false,
+		"Strip ANSI color codes from the output for clean log files",
+	)
 
 	colorFlags := func(s string) string {
 		re := regexp.MustCompile(`(?m)(^\s+-[a-zA-Z], --[a-zA-Z0-9-]+|^\s+--[a-zA-Z0-9-]+)`)
